1c38c505f25de003fcb1aa132084fdbb11ef2a0f
Faiza-Mazhar <faiza.mazhar@bbc.co.uk>
2020-10-16 17:15:33 +0100

    Added Search Box Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import CardList from "./component/CradList/cardList";
+import SearchBox from "./component/SearchBox/searchBox";
 
 function App() {
   const [monsters, setMonsters] = useState([]);
+  const [searchFieldInput, setSearchFieldInput] = useState("");
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => setMonsters([...users, ...users]));
+      .then((users) => setMonsters(users));
   }, []);
+
+  const modifiedMonsters = monsters.filter((monster) =>
+    monster.name.toLowerCase().includes(searchFieldInput.toLowerCase())
+  );
 
   return (
     <div className="App">
-      <CardList monsters={monsters} />
+      {console.log(monsters)}
+      <SearchBox
+        placeholder="Search Monsters"
+        handleChange={(event) => setSearchFieldInput(event.target.value)}
+      />
+      <CardList monsters={modifiedMonsters} />
     </div>
   );
 }

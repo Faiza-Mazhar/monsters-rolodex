6e7e7440a607ef3b1a74ff35b79aa233261f6989
Faiza-Mazhar <faiza.mazhar@bbc.co.uk>
2020-10-16 14:24:36 +0100

    Removed mosters from the useEffect second parameter to stop infinte fetching, added empty component for CardList and Card

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ function App() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => setMonsters(users));
-  }, [monsters]);
+  }, []);
 
   return (
     <div className="App">
       {monsters.map((monster) => {
         console.log("monster", monster);
-        return <h2 key={monster.id}>{monster.name}</h2>;
+        return <h1 key={monster.id}>{monster.name}</h1>;
       })}
     </div>
   );
---
+++ src/component/CradList/cardList.jsx
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from "react";
+
+export const CardList = (props) => {
+  return <div>{console.log(props)}</div>;
+};

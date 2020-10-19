abc5a60319ac86e32e77e049e243ff0a92c3d450
Faiza-Mazhar <faiza.mazhar@bbc.co.uk>
2020-10-19 14:51:37 +0100

    Updates

---
+++ precache-manifest.5526ccf1450bf950a1cab03d195b59be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12ffba43235bfa9065495b17270b9510",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3b027c7337a10b2cb9bc",
+    "url": "/monsters-rolodex/static/css/main.9eb03bf0.chunk.css"
+  },
+  {
+    "revision": "e925f219b5654bb5eff5",
+    "url": "/monsters-rolodex/static/js/2.cd02af53.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.cd02af53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b027c7337a10b2cb9bc",
+    "url": "/monsters-rolodex/static/js/main.2929e2c8.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
